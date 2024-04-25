d277dc677de0bb0ce05a9fd73689f530181e15ba
Sam <74820069+the-gribble@users.noreply.github.com>
2024-04-25 09:09:23 +0200

    Update database.py

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://sa_ahs_db:Password123#@db/mydatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@db/mydatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
